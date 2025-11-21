8c4a3b71eb6742b1b17a9299e381a4a51b61a7cd
Niyasperumannil <niyaskoottilakkadavu@gmail.com>
2025-11-21 16:04:44 +0530

    meesage'

--- src/components/VideoHero/VideoHero.jsx
+++ src/components/VideoHero/VideoHero.jsx
@@ -1,15 +1,15 @@
-// import React from "react";
-// import "./VideoHero.css";
+import React from "react";
+import "./VideoHero.css";
 
-// const VideoOnlySection = () => {
-//   return (
-//     <section className="vos-section">
-//       <video className="vos-video" autoPlay muted loop playsInline>
-//         <source src="https://www.solomia-home.ae/storage/versace-site-16-9.mp4" type="video/mp4" />
-//         Your browser does not support the video tag.
-//       </video>
-//     </section>
-//   );
-// };
+const VideoOnlySection = () => {
+  return (
+    <section className="vos-section">
+      <video className="vos-video" autoPlay muted loop playsInline>
+        <source src="https://www.solomia-home.ae/storage/versace-site-16-9.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </section>
+  );
+};
 
-// export default VideoOnlySection;
+export default VideoOnlySection;
